846e4bb6ed1ea49027c204e91d41bea637706984
Takaaki Masaki <takaakimasaki@users.noreply.github.com>
2023-02-06 22:52:43 +0100

    addressed bug in the code preventing the generation of a map

--- inst/shiny-examples/eviatlas/server.R
+++ inst/shiny-examples/eviatlas/server.R
@@ -665,7 +665,7 @@ shinyServer(
     })
 
     cluster_options <- reactive({
-      if_else(input$map_cluster_select,
+      ifelse(input$map_cluster_select,
               parse(text=paste0('markerClusterOptions(freezeAtZoom = ',
                                 input$cluster_size_select, ",spiderfyDistanceMultiplier=2" ,')')),
               NULL)
